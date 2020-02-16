c91f17952df84694d2c83abaca4edf75952b5a7d
Rob Douma <rdouma81@gmail.com>
2020-02-16 11:29:54 -0500

    ViewModel implementation.

--- app/src/main/java/com/example/simplechef/ui/home/AllRecipesFragment.java
+++ app/src/main/java/com/example/simplechef/ui/home/AllRecipesFragment.java
@@ -1,14 +1,30 @@
 package com.example.simplechef.ui.home;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.simplechef.data.AllRecipesViewModel;
 
 public class AllRecipesFragment extends Fragment {
     private static final String TAG = "AllRecipesFragment";
+    private AllRecipesViewModel allRecipesViewModel;
 
     public static AllRecipesFragment newInstance() {
         AllRecipesFragment fragment = new AllRecipesFragment();
         return fragment;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        allRecipesViewModel = new ViewModelProvider(this).get(AllRecipesViewModel.class);
+        allRecipesViewModel.getRecipes().observe(this, recipes -> {
+            // update UI
+        });
+
+    }
 }
