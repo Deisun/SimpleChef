b86af06fc5375eb951e70f96476930c328fb09db
Rob Douma <rdouma81@gmail.com>
2020-02-16 11:00:53 -0500

    ViewModel implementation.

---
+++ app/src/main/java/com/example/simplechef/data/AllRecipesViewModel.java
@@ -0,0 +1,47 @@
+package com.example.simplechef.data;
+
+import android.app.Application;
+
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+public class AllRecipesViewModel extends AndroidViewModel {
+    private RecipeRepository repository;
+    private LiveData<List<Recipe>> recipes;
+
+    public AllRecipesViewModel(@NonNull Application application) {
+        super(application);
+        repository = new RecipeRepository(application);
+        recipes = repository.getAllRecipes();
+    }
+
+    public LiveData<List<Recipe>> getRecipes() {
+        if (recipes == null) {
+            recipes = new MutableLiveData<>();
+        }
+        return recipes;
+    }
+
+    private void loadRecipes() {
+        //asynchronously load all recipes
+    }
+
+    public void insert(Recipe recipe) {
+        repository.insert(recipe);
+    }
+
+    public void update(Recipe recipe) {
+        repository.update(recipe);
+    }
+
+    public void delete(Recipe recipe) {
+        repository.delete(recipe);
+    }
+
+
+}
