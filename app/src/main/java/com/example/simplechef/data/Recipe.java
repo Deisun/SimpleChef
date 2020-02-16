0b1edf80c6c76837b9f54e9acdde14b90a2cb537
Rob Douma <rdouma81@gmail.com>
2020-02-15 23:03:28 -0500

    Entity setup

---
+++ app/src/main/java/com/example/simplechef/data/Recipe.java
@@ -0,0 +1,35 @@
+package com.example.simplechef.data;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "recipe_table")
+public class Recipe {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String title;
+    private String description;
+
+    public Recipe(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
