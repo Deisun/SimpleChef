e759c10b75e4b34202db5e11d31b7a482c894043
Rob Douma <rdouma81@gmail.com>
2020-02-16 00:28:25 -0500

    Repository setup

--- app/src/main/java/com/example/simplechef/data/RecipeDatabase.java
+++ app/src/main/java/com/example/simplechef/data/RecipeDatabase.java
@@ -1,10 +1,13 @@
 package com.example.simplechef.data;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = Recipe.class, version=1)
 public abstract class RecipeDatabase extends RoomDatabase {
@@ -18,8 +21,31 @@ public abstract class RecipeDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     RecipeDatabase.class, "recipe_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
         return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private RecipeDao recipeDao;
+
+        private PopulateDbAsyncTask(RecipeDatabase db) {
+            recipeDao = db.recipeDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            recipeDao.insert(new Recipe("Recipe 1", "This is the first recipe"));
+            return null;
+        }
     }
 }
--- app/src/main/java/com/example/simplechef/data/RecipeRepository.java
+++ app/src/main/java/com/example/simplechef/data/RecipeRepository.java
@@ -18,15 +18,16 @@ public class RecipeRepository {
     }
 
     public void insert(Recipe recipe) {
+        new InsertRecipeAsyncTask(recipeDao).execute(recipe);
 
     }
 
     public void update(Recipe recipe) {
-
+        new UpdateRecipeAsyncTask(recipeDao).execute(recipe);
     }
 
     public void delete(Recipe recipe) {
-
+        new DeleteRecipeAsyncTask(recipeDao).execute(recipe);
     }
 
     public LiveData<List<Recipe>> getAllRecipes() {
@@ -34,6 +35,42 @@ public class RecipeRepository {
     }
 
     private static class InsertRecipeAsyncTask extends AsyncTask<Recipe, Void, Void> {
+        private RecipeDao recipeDao;
+
+        private InsertRecipeAsyncTask(RecipeDao recipeDao) {
+            this.recipeDao = recipeDao;
+        }
+        @Override
+        protected Void doInBackground(Recipe... recipes) {
+            recipeDao.insert(recipes[0]);
+            return null;
+        }
+    }
+
+    private static class UpdateRecipeAsyncTask extends AsyncTask<Recipe, Void, Void> {
+        private RecipeDao recipeDao;
+
+        private UpdateRecipeAsyncTask(RecipeDao recipeDao) {
+            this.recipeDao = recipeDao;
+        }
+        @Override
+        protected Void doInBackground(Recipe... recipes) {
+            recipeDao.update(recipes[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteRecipeAsyncTask extends AsyncTask<Recipe, Void, Void> {
+        private RecipeDao recipeDao;
+
+        private DeleteRecipeAsyncTask(RecipeDao recipeDao) {
+            this.recipeDao = recipeDao;
+        }
+        @Override
+        protected Void doInBackground(Recipe... recipes) {
+            recipeDao.delete(recipes[0]);
+            return null;
+        }
 
     }
 }
