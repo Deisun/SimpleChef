77120dd0ea708a8e4cdae8b9edccb6bf213bb357
Deisun <rdouma81@gmail.com>
2020-02-17 17:13:41 -0500

    Fixing ViewModel with RecyclerView

--- app/src/main/java/com/example/simplechef/ui/home/AllRecipesFragment.java
+++ app/src/main/java/com/example/simplechef/ui/home/AllRecipesFragment.java
@@ -1,17 +1,24 @@
 package com.example.simplechef.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.simplechef.R;
 import com.example.simplechef.data.AllRecipesViewModel;
+import com.example.simplechef.ui.recipe_view.ViewRecipeActivity;
 
 public class AllRecipesFragment extends Fragment {
     private static final String TAG = "AllRecipesFragment";
     private AllRecipesViewModel allRecipesViewModel;
+    private RecipeListAdapter recipeListAdapter;
+    private RecyclerView recyclerView;
 
     public static AllRecipesFragment newInstance() {
         AllRecipesFragment fragment = new AllRecipesFragment();
@@ -22,10 +29,37 @@ public class AllRecipesFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         allRecipesViewModel = new ViewModelProvider(this).get(AllRecipesViewModel.class);
         allRecipesViewModel.getRecipes().observe(this, recipes -> {
             // update RecyclerView
-            Toast.makeText(getActivity(), "onChanged", Toast.LENGTH_LONG).show();
+
+        });
+
+        recyclerView = fragView.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        recipeListAdapter = new RecipeListAdapter(recipeObject, favoritesList);
+        recyclerView.setAdapter(recipeListAdapter);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), 1);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        recipeListAdapter.setOnItemClickListener(new RecipeListAdapter.OnRecipeItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                Intent intent = recipeObject.get(position).toIntent(getActivity(), ViewRecipeActivity.class);
+
+                startActivity(intent);
+            }
+
+            @Override
+            public void onFavoriteItemClick(int position) {
+                favoritesList.remove(position);
+                recipeListAdapter.notifyDataSetChanged();
+            }
+
+
         });
     }
 }
